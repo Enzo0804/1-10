9e737cf8cd01b72860e10a83be144afdf67036d5
ENZO GONCALVES SANTOS <enzo.gsantos@senacsp.edu.br>
2024-10-01 17:19:08 -0300

    first commit

---
+++ EXERCICIO_4/script.js
@@ -0,0 +1,14 @@
+let contador = 1;
+
+while (contador <= 10) {
+    if (contador % 2 == 0){
+        console.log(contador = " é par") ;
+    }else{
+        console.log(contador = " é impar")
+    }
+    contador++;
+}
+//for (let index = 0; index < array.length; index++) {
+    ///const element = array[index];
+    
+//}
